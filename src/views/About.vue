c2f6e922a0519efb764bb84ba2ae11d171b2402a
BlueSCar <radjewwj@gmail.com>
2018-12-23 15:57:39 -0500

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="about">
-        <b-jumbotron>
-            <h1>More information forthcoming...</h1>
-        </b-jumbotron>
+        <h1>About this site</h1>
+        <p class="lead">The data from this site is gathered using a variety of sources and is ever expanding.  This site is built on top of our public API, <a href='https://api.collegefootballdata.com' target="_blank">api.collegefootballdata.com</a>.  The API is built and data gathered using a variety of open source tools found on my <a href='https://github.com/bluescar' target="_blank">Github page</a>.  Please visit the <router-link to="/contribute">Contribution page</router-link> for ways to contribute.  You can also contact me a <a href='mailto:admin@collegefootballdata.com'>admin@collegefootballdata.com</a>.</p>
     </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
