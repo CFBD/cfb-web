92d2bfb8efca9e87074c4927e4816fc2c1649be6
Bill Radjewski <radjewwj@gmail.com>
2023-08-10 21:49:38 -0400

    ghost content api key change

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -7,43 +7,43 @@
 </template>
 
 <script>
-    import BlogPost from './BlogPost.vue';
+import BlogPost from './BlogPost.vue';
 
-    export default {
-        components: {
-            BlogPost
-        },
-        data() {
-            return {
-                posts: []
-            };
-        },
-        created() {
-            const api = new GhostContentAPI({
-                url: 'https://blog.collegefootballdata.com',
-                key: '3d654ae56e824a1b9001763e9e',
-                version: 'v2'
+export default {
+    components: {
+        BlogPost
+    },
+    data() {
+        return {
+            posts: []
+        };
+    },
+    created() {
+        const api = new GhostContentAPI({
+            url: 'https://blog.collegefootballdata.com',
+            key: '027d8b9823dfc75ab0c1b9c2ca',
+            version: 'v2'
+        });
+        api.posts
+            .browse({
+                limit: 6,
+                include: 'tags'
+            })
+            .then((posts) => {
+                this.posts = posts.map(post => ({
+                    title: post.title,
+                    subtitle: post.custom_excerpt,
+                    href: post.url,
+                    img: post.feature_image
+                }));
             });
-            api.posts
-                .browse({
-                    limit: 6,
-                    include: 'tags'
-                })
-                .then((posts) => {
-                    this.posts = posts.map(post => ({
-                        title: post.title,
-                        subtitle: post.custom_excerpt,
-                        href: post.url,
-                        img: post.feature_image
-                    }));
-                });
-        }
-    };
+    }
+};
 
 </script>
 
 <style lang='scss'>
-    #blog-box {
-        padding: 2em;
-    }
+#blog-box {
+    padding: 2em;
+}
 </style>
